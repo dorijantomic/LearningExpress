b254e0839ad2b96116d38d1710940433b2f96784
dorijantomic <dorijan11@gmail.com>
2023-11-11 16:36:07 +0100

    initial commit

--- NodeJS/models/product.js
+++ NodeJS/models/product.js
@@ -1,95 +1,107 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
 const productSchema = new Schema({
   title: {
     type: String,
-    required: true,
+    required: true
   },
   price: {
     type: Number,
-    required: true,
+    required: true
   },
   description: {
     type: String,
-    required: true,
+    required: true
   },
   imageUrl: {
     type: String,
-    required: true,
+    required: true
   },
   userId: {
     type: Schema.Types.ObjectId,
-    ref: "User", // relate user id to the userId of a user model ie relation
-    required: true,
-  },
+    ref: 'User',
+    required: true
+  }
 });
 
-module.exports = mongoose.model("Product", productSchema);
-// const { ObjectId } = require("mongodb");
-// const getDb = require("../util/database").getDb;
+module.exports = mongoose.model('Product', productSchema);
+
+// const mongodb = require('mongodb');
+// const getDb = require('../util/database').getDb;
+
 // class Product {
 //   constructor(title, price, description, imageUrl, id, userId) {
 //     this.title = title;
 //     this.price = price;
 //     this.description = description;
 //     this.imageUrl = imageUrl;
-//     this._id = id ? new ObjectId(id) : null;
+//     this._id = id ? new mongodb.ObjectId(id) : null;
 //     this.userId = userId;
 //   }
-//   static fetchAll() {
-//     const db = getDb();
-//     return db
-//       .collection("products")
-//       .find()
-//       .toArray()
-//       .then((products) => {
-//         console.log(products);
-//         return products;
-//       })
-//       .catch((err) => console.error(err)); // returns all of data not good for large dataset
-//   }
 
-//   static findById(prodId) {
-//     const db = getDb();
-//     return db
-//       .collection("products")
-//       .find({ _id: new ObjectId(prodId) })
-//       .next()
-//       .then((product) => {
-//         console.log(product);
-//         return product;
-//       });
-//   }
 //   save() {
 //     const db = getDb();
 //     let dbOp;
 //     if (this._id) {
 //       // Update the product
 //       dbOp = db
-//         .collection("products")
+//         .collection('products')
 //         .updateOne({ _id: this._id }, { $set: this });
 //     } else {
-//       dbOp = db.collection("products").insertOne(this);
+//       dbOp = db.collection('products').insertOne(this);
 //     }
 //     return dbOp
-//       .then((result) => {
+//       .then(result => {
 //         console.log(result);
 //       })
-//       .catch((err) => {
-//         console.error(err);
+//       .catch(err => {
+//         console.log(err);
 //       });
 //   }
+
+//   static fetchAll() {
+//     const db = getDb();
+//     return db
+//       .collection('products')
+//       .find()
+//       .toArray()
+//       .then(products => {
+//         console.log(products);
+//         return products;
+//       })
+//       .catch(err => {
+//         console.log(err);
+//       });
+//   }
+
+//   static findById(prodId) {
+//     const db = getDb();
+//     return db
+//       .collection('products')
+//       .find({ _id: new mongodb.ObjectId(prodId) })
+//       .next()
+//       .then(product => {
+//         console.log(product);
+//         return product;
+//       })
+//       .catch(err => {
+//         console.log(err);
+//       });
+//   }
+
 //   static deleteById(prodId) {
 //     const db = getDb();
 //     return db
-//       .collection("products")
-//       .deleteOne({ _id: new ObjectId(prodId) })
-//       .then((result) => {
-//         console.log("Product deleted!");
+//       .collection('products')
+//       .deleteOne({ _id: new mongodb.ObjectId(prodId) })
+//       .then(result => {
+//         console.log('Deleted');
 //       })
-//       .catch((err) => console.error(err));
+//       .catch(err => {
+//         console.log(err);
+//       });
 //   }
 // }
 
